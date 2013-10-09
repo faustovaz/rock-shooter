a015b8bb45e5c01c1cb590188e1e9b3d80fd70db
Jefferson Fausto Vaz <faustovaz@gmail.com>
2013-10-09 00:06:50 +0000

    Implementing explosion animation

--- rockshooter/enemy.py
+++ rockshooter/enemy.py
@@ -8,19 +8,18 @@ class Enemy:
 	def __init__(self):
 		self.image = pygame.image.load("images/enemy_1.png")
 		self.sprites = self._getSpriteSequence()
+		self.spriteIndex = 0
 		self.position = self.getRandomPosition()
 		self.isVisible = True
 		self.background = pygame.Surface((48, 50)).convert()
-		self.spriteIndex = 0
+		self.explosionImage = pygame.image.load("images/enemy_explosion.png")
+		self.explosionSprites = self._getExplosionSpriteSequence()
 
 	def move(self):
 		if (self.position[1] + 1 < game.Game.screenSize[1]):
 			self.position = (self.position[0], self.position[1] + 1)
 		else:
 			self.isVisible = False
-
-	def updateSprites(self):
-		pass
 
 	def _getSpriteSequence(self):
 		sprites = []
@@ -37,5 +36,26 @@ class Enemy:
 			self.spriteIndex = 0
 
 	def getRandomPosition(self):
+
 		randomX = random.randint(10, game.Game.screenSize[0] - 10)
 		return (randomX, 0)
+
+	def explode(self):
+		pass
+
+	def _getExplosionSpriteSequence(self):
+		explosionSprites = []
+		explosionClips = [
+			[ 0,   0, 48, 48],
+			[ 48,  0, 48, 48],
+			[ 96,  0, 48, 48],
+			[144,  0, 48, 48],
+			[  0, 48, 48, 48],
+			[ 48, 48, 48, 48],
+			[ 96, 48, 48, 48],
+			[144, 48, 48, 48],
+		]
+		for clip in explosionClips:
+			self.explosionImage.set_clip(pygame.Rect(clip[0], clip[1], clip[2], clip[3]))
+			explosionSprites.append(self.explosionImages.subsurface(self.image.get_clip()))
+		return explosionSprites
